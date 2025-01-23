8f541db498985c1e91188430fbeff9f5f02bfc50
Rafael Velazco <rjvelazco21@gmail.com>
2025-01-23 13:13:25 -0400

    feat: initialize dotCMS client & get Page Asset

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useEffect, useState } from 'react'
+import { DotCmsClient } from '@dotcms/client';
+
+const client = DotCmsClient.init({
+  dotcmsUrl: `${import.meta.env.VITE_DOTCMS_HOST_KEY}`,
+  authToken: `${import.meta.env.VITE_DOTCMS_AUTH_TOKEN_KEY}`,
+  siteId: `${import.meta.env.VITE_DOTCMS_SITE_ID_KEY}`
+});
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pageAsset, setPageAsset] = useState(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  useEffect(() => {
+    client.page
+      .get({ path: "/example", language_id: 1 })
+      .then((pageAsset) => {
+        console.log("DOTCMS PAGE ASSET:", pageAsset);
+        setPageAsset(pageAsset);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  return <div>EMPTY Example</div>;
 }
 
 export default App
