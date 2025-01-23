e357feb8d6993fcd411f430425cbeb36f7d17f96
Rafael Velazco <rjvelazco21@gmail.com>
2025-01-23 12:58:04 -0400

    feat: define .env Variable

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-})
+  define: {
+    'process.env.VITE_DOTCMS_HOST': JSON.stringify("VITE_DOTCMS_HOST_KEY"),
+    'process.env.VITE_DOTCMS_AUTH_TOKEN_KEY': JSON.stringify("VITE_DOTCMS_AUTH_TOKEN_KEY"),
+    'process.env.VITE_DOTCMS_SITE_ID_KEY': JSON.stringify("VITE_DOTCMS_SITE_ID_KEY"),
+  }
+});
